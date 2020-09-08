383ef675abcbf5c932b600b24866bd38a96ce289
Rahul Ajith <rahulajithkumar1@gmail.com>
2020-09-08 13:36:38 +0530

    Final Timer

--- src/App.js
+++ src/App.js
@@ -1,54 +1,76 @@
-import React,{useRef, useState, useEffect} from 'react';
 
-const App = () => {
-    const [timerDays, setTimerDays] = useState('00');
-    const [timerHours, setTimerHours] = useState('00');
-    const [timerMinutes, setTimerMinutes] = useState('00');
-    const [timerSeconds, setTimerSeconds] = useState('00');
+import React,{useState, useEffect, useRef} from 'react';
+import moment,{duration} from 'moment';
 
 
+const Timer = ({quizTimer, timeOverResultModalHandler,fetchTime}) => {
 
-let interval = useRef();
+    
+    //timer States
+    const [timerHours, setTimerHours] = useState(0);
+    const [timerMinutes, setTimerMinutes] = useState(0);
+    const [timerSeconds, setTimerSeconds] = useState(0);
+    const [timerSum, setTimerSum] = useState(null)
+    //end timer states
 
+//    if(isNaN(timerHours)===true && isNaN(timerMinutes)===true && isNaN(timerSeconds)===true ){
+//     localStorage.setItem("timer", moment().add(localStorage.getItem("time") , 'hours').format())
+//    }
 
-const startTimer = () => {
-    //Give below the railway time and Date
-    const countDownDate = new Date("Aug 29, 2020 17:25:00").getTime();
+    let interval = useRef();
+    const currentInterval = interval.current
+     
+     
+  const startTimer = () => {
 
     interval = setInterval(() => {
-        const now = new Date().getTime();
-        const distance = countDownDate - now;
+        const countDownDate = moment("2020-09-01 12:27:00")
+        const now = moment().format()
+        const distance = duration(countDownDate.diff(now))
 
-        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        const hours =  Math.floor((distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
-        const minutes = Math.floor((distance % (1000 * 60 * 60))/(1000*60));
-        const seconds = Math.floor((distance % (1000 * 60))/1000);
+        const hours = distance.hours();
+        const minutes = distance.minutes();
+        const seconds = distance.seconds();
+        const timerAdd = hours + minutes + seconds
 
         if(distance < 0){
-            clearInterval(interval.current);
+            clearInterval(currentInterval);
+            
         }else{
-            setTimerDays(days);
             setTimerHours(hours);
             setTimerMinutes(minutes);
             setTimerSeconds(seconds);
+            setTimerSum(timerAdd)
         }
-
     }, 1000)
 }
 
     useEffect(() => {
         startTimer();
         return () => {
-            clearInterval(interval.current);
+            clearInterval(currentInterval);
+            
         }
     });
+   
+   //timerEnds
+ 
+// timeOverResultModalHandler(timerSum);
+// resultModalHandler(timerSeconds, timerMinutes, timerHours);
+
+    // let timerInfo = ( )
+    // if(timerHours===null && timerMinutes ===null && timerSeconds===null ){
+    //     timerInfo=(<p>0:00:00</p>)
+    // }
 
     return(
-        <div style={{textAlign: 'center', fontSize: 50, fontWeight: 600}}>
-            <p>{timerDays} : {timerHours} : {timerMinutes}: {timerSeconds}</p>
+        <div className="Timer">
+           <p>{timerHours}:{timerMinutes<10 ? `0${timerMinutes}` : timerMinutes}:{timerSeconds<10 ? `0${timerSeconds}`:timerSeconds}</p>
+        {/* {timerHours && <p>{timerHours}</p>}: */}
         </div>
+       
     )
 
     }
 
-export default App;
+export default Timer;
--- src/components/differentTimer.js
+++ src/components/differentTimer.js
@@ -52,3 +52,65 @@ if(hours ===0 && minutes === 0 && seconds === 0){
         )
     }
 }
+
+
+
+
+                                        //timer 
+
+
+
+                                        import React,{useRef, useState, useEffect} from 'react';
+
+                                        const App = () => {
+                                            const [timerDays, setTimerDays] = useState('00');
+                                            const [timerHours, setTimerHours] = useState('00');
+                                            const [timerMinutes, setTimerMinutes] = useState('00');
+                                            const [timerSeconds, setTimerSeconds] = useState('00');
+                                        
+                                        
+                                        
+                                        let interval = useRef();
+                                        
+                                        
+                                        const startTimer = () => {
+                                            //Give below the railway time and Date
+                                            const countDownDate = new Date("sept 1, 2020 10:59:00").getTime();
+                                        
+                                            interval = setInterval(() => {
+                                                const now = new Date().getTime();
+                                                const distance = countDownDate - now;
+                                        
+                                                const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                                                const hours =  Math.floor((distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
+                                                const minutes = Math.floor((distance % (1000 * 60 * 60))/(1000*60));
+                                                const seconds = Math.floor((distance % (1000 * 60))/1000);
+                                        
+                                                if(distance < 0){
+                                                    clearInterval(interval.current);
+                                                }else{
+                                                    setTimerDays(days);
+                                                    setTimerHours(hours);
+                                                    setTimerMinutes(minutes);
+                                                    setTimerSeconds(seconds);
+                                                }
+                                        
+                                            }, 1000)
+                                        }
+                                        
+                                            useEffect(() => {
+                                                startTimer();
+                                                return () => {
+                                                    clearInterval(interval.current);
+                                                }
+                                            });
+                                        
+                                            return(
+                                                <div style={{textAlign: 'center', fontSize: 50, fontWeight: 600}}>
+                                                    <p>{timerDays} : {timerHours} : {timerMinutes}: {timerSeconds}</p>
+                                                </div>
+                                            )
+                                        
+                                            }
+                                        
+                                        export default App;
